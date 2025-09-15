600d5be5d58c61409bf4b008229bac33b0f6aee0
Isaac <mayne.isaac@gmail.com>
2025-09-15 22:57:58 +0200

    estilos de header y seccion hero

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,12 @@ import Header from './components/Header.vue'
   <Header />
   <main class="mt-16">
     <section class="background-hero">
-      <h1>Prueba Técnica</h1>
-      <h2>Formularios dinámicos</h2>
+      <div class="container p-10 max-w-7xl mx-auto flex">
+        <div class="flex flex-col gap-2 text-center md:text-left w-full">
+          <h1 class="text-white text-4xl md:text-6xl">Prueba Técnica</h1>
+          <h2 class="text-secondary text-4xl md:text-6xl">Formularios dinámicos</h2>
+        </div>
+      </div>
     </section>
   </main>
   <footer></footer>
